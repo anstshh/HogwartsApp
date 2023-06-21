b17a6f5e89b207d005631ef369c7a0189cffd1e4
Admin <anstdor@gmail.com>
2023-06-21 09:03:25 +0300

    Initial commit

---
+++ src/main/java/com/example/hogwartsapp/HogwartsAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.hogwartsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogwartsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HogwartsAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hogwartsapp/HogwartsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hogwartsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogwartsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
